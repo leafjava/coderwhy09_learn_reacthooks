747341eba6cb6d6201d96f36cf5f2b5402bc2043
LAPTOP-KCF3SC5G\HUAWEI <a894074295@126.com>
2025-08-12 13:53:08 +0800

    1577_day87_Redux核心原理-React路由-Hooks-16_(掌握)Hooks-useState的使用详解和使用规

--- src/02_计数器实现对比/CounterHook.jsx
+++ src/02_计数器实现对比/CounterHook.jsx
@@ -1,7 +1,21 @@
 import {memo,useState} from "react"
 
+// 普通函数里面不能使用hooks
+// function foo(){
+//   const [message] = useState("Hello world")
+// }
+
+// 在自定义的hooks中,可以使用react提供的其他hooks,必须使用use开头
+function useFoo(){
+  const [message] = useState("Hello world")
+  return message
+}
+
 function CounterHook(props){
   const [counter, setCounter] = useState(0)
+
+  // foo()
+  const message = useFoo()
 
   return (
     <div>
