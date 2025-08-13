22d74fd453e8b33d0d479ce08aa213368c6222b3
LAPTOP-KCF3SC5G\HUAWEI <a894074295@126.com>
2025-08-13 18:12:31 +0800

    1586_day88_React Hooks使用和自动自定义Hooks-07_(了解)Hooks-useReducer管理复杂的数据

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import ReactDOM from 'react-dom/client';
 // import App from './02_计数器实现对比/App';
 // import App from './03_useState的使用/App';
 // import App from './04_useEffect的使用/App';
-import App from './05_useContext的使用/App';
+// import App from './05_useContext的使用/App';
+import App from './06_useReducer的使用(了解)/App';
+
+
 import {UserContext,ThemeContext} from "./05_useContext的使用/context"
 
 
