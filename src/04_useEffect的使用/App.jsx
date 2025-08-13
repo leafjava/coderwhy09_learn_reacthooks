82a2338583dd136c143db467625084d520f28f9a
LAPTOP-KCF3SC5G\HUAWEI <a894074295@126.com>
2025-08-13 09:48:28 +0800

    1580_day88_React Hooks使用和自动自定义Hooks-01_(掌握)Hooks-修改标题案例-useEffect的

--- src/04_useEffect的使用/App.jsx
+++ src/04_useEffect的使用/App.jsx
@@ -3,11 +3,12 @@ import React, { memo, useState, useEffect } from 'react'
 const App = memo(() => {
   const [count,setCount] = useState(200)
 
-  document.title = count
+  
 
   useEffect(() => {
     // 当前传入的回调函数会在组件被渲染完成后,自动执行
     // 网络请求/DOM操作(修改标题)/事件监听
+    document.title = count
   })
 
   return (
