d0b6f3a78b758fac211ec4e49fc6516e704eb972
LAPTOP-KCF3SC5G\HUAWEI <a894074295@126.com>
2025-08-14 10:25:46 +0800

    1588_day88_React Hooks使用和自动自定义Hooks-09_(理解)Hooks-useCallback记忆值的性能

--- src/07_useCallback和useMemo/App.jsx
+++ src/07_useCallback和useMemo/App.jsx
@@ -1,21 +1,74 @@
 import React, { memo, useCallback, useState } from 'react'
 
+// useCallback性能优化的点:
+// 1.当需要将一个函数传递给子组件时,最好使用useCallback进行优化,将优化之后的函数传递给子组件
+
+// props中的属性发生改变时,组件本身就会被重新渲染
+const HYHome = memo(function(props) {
+  const {increment} = props
+
+  console.log("HYHome被渲染")
+
+  return (
+    <div>
+      <button onClick={increment}>increment+1</button>
+
+      {/* 100个子组件 */}
+    </div>
+  )
+})
+
 const App = memo(function(){
   const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("hello")
 
   // const increment = e => {
   //   setCount(count + 1)
   // }
-  function increment(){
+  // function increment(){
+  //   setCount(count + 1)
+  // }
+
+  // 闭包陷阱 useCallback
+  const increment = useCallback(function foo(){
     setCount(count + 1)
-  }
+    console.log("increment")
+  },[count])
+
+  // 普通函数
+  // const increment = () => {
+  //   setCount(count + 1)
+  // }
 
   return (
     <div>
       <h2>计数:{count}</h2>
       <button onClick={increment}>+1</button>
+
+      <HYHome increment={increment}/>
+
+      <h2>message:{message}</h2>
+      <button onClick={e => setMessage(Math.random())}>修改message</button>
     </div>
   )
 })
+
+// 闭包
+// function foo(name){
+
+//   function bar(){
+//     console.log(name)
+//   }
+
+//   return bar
+// }
+
+// const bar1 = foo("why")
+// bar1() //why
+
+// const bar2 = foo("kobe")
+// bar2() //kobe
+
+// bar1() //why
 
 export default App
